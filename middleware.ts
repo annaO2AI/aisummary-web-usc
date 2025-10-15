64247ce94644878a3981508face7bb9d4a7a9637
Sravani-Ravulla <Ravula.Sravani@elevancesystems.com>
2025-10-15 20:47:49 +0530

    updated the url

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export function middleware(request: NextRequest) {
 
   if (!isPublicPath && !token) {
     const redirectTo = `${request.nextUrl.origin}/auth/callback`
-    const loginUrl = `https://aisummary-api-usc-geemebfqfmead8f4.centralus-01.azurewebsites.net?redirect_uri=${encodeURIComponent(
+    const loginUrl = `https://ai-call-summary-api-hpb0afdgbtb6e5ca.centralus-01.azurewebsites.net/?redirect_uri=${encodeURIComponent(
       redirectTo
     )}`
     return NextResponse.redirect(loginUrl)
